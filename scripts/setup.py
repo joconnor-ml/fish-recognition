1f9cc190fd642867570dcb73f79292234914a565
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-03-10 22:00:24 +0000

    trying packaging up dependencies again

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,9 +2,15 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+REQUIRED_PACKAGES = [
+    "keras",
+    "theano",
+]
+
 setup(
     name='dataflow_preprocess',
     version='1.0',
     description='Image prepocessing code to be run on Google Dataflow',
     packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,
 )
